9892fbf0381cea5b4b136055b99d2dbb66442ce7
danielecalderon <daniel.calderon06@gmail.com>
2020-04-17 15:45:55 +0000

    done with blog!

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200413140917) do
+ActiveRecord::Schema.define(version: 20200417150313) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
   end
 
 end
