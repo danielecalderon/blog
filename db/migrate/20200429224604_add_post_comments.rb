256123192ef852604a0c468316d0a996f7a43c9a
danielecalderon <daniel.calderon06@gmail.com>
2020-05-10 20:05:31 +0000

    Active Record Associations Done

---
+++ db/migrate/20200429224604_add_post_comments.rb
@@ -0,0 +1,4 @@
+class AddPostComments < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200418043338) do
+ActiveRecord::Schema.define(version: 20200429224646) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "post_id"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "pages", force: :cascade do |t|
     t.string   "title"
